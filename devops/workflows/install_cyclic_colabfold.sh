86bc0bfe8e12fee27ef76270c6eab3e79fa64a6c
Griffin Young <griffin.young@schrodinger.com>
2023-09-18 19:28:48 -0400

    added gsutil copy

--- devops/workflows/install_cyclic_colabfold.sh
+++ devops/workflows/install_cyclic_colabfold.sh
@@ -8,3 +8,5 @@ export SITEPACKAGES="${COLABFOLDDIR}/colabfold-conda/lib/python3.10/site-package
 printf "\ndef add_cyclic_offset(offset, L):\n  max_dist = (L // 2)\n  idxs = jnp.arange(offset.shape[0])\n  i = idxs[:,None]\n  j = idxs[None,:]\n  set_indices = (i < L) & (j < L)\n  dists = abs(j - i)\n  dists = jnp.where((dists > max_dist), L - dists, dists)\n  upper_right = (i < j)\n  offset = jnp.where(set_indices & upper_right, -dists, offset)\n  offset = jnp.where(set_indices & ~upper_right, dists, offset)\n  return offset" >> $SITEPACKAGES/alphafold/model/utils.py
 sed -i "s/offset = pos\[:, None\] - pos\[None, :\]/offset = pos[:, None] - pos[None, :]\n    binder_len=jnp.sum(asym_id == 0)\n    offset = utils.add_cyclic_offset(offset,binder_len)\n/g" $SITEPACKAGES/alphafold/model/modules_multimer.py
 sed -i "s/offset = pos\[:,None\] - pos\[None,:\]/offset = pos[:,None] - pos[None,:]\n\toffset = utils.add_cyclic_offset(offset,len(pos))\n/g" $SITEPACKAGES/alphafold/model/modules.py
+
+gsutil -m cp -r $COLABFOLDDIR gs://alphafold-environments/cyclic_peptide_environment
