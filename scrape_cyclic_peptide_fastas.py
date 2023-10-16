630dc20ba81cfa9ad50b073ebafcfe950feba9d0
Griffin Young <griffin.young@schrodinger.com>
2023-10-16 18:24:30 -0400

    Add peptide scraping code

--- scrape_cyclic_peptide_fastas.py
+++ scrape_cyclic_peptide_fastas.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import requests
 
+
 def get_sequence(pdb_id):
     url = 'https://www.rcsb.org/fasta/entry/' + pdb_id
     response = requests.get(url)
@@ -9,6 +10,7 @@ def get_sequence(pdb_id):
     fasta_str = response.text
     sequence = fasta_str.split('\n')[1]
     return sequence
+
 
 df = pd.read_csv('supplementary_table_1.csv')
 pdb_ids = df['PDB'].tolist()
