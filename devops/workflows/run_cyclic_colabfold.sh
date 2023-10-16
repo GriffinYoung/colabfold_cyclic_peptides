85e8479bbf927e8cdd485ae33311615495db42e6
Griffin Young <griffin.young@schrodinger.com>
2023-10-16 18:25:37 -0400

    Adding subdirs arg to colabfold

--- devops/workflows/run_cyclic_colabfold.sh
+++ devops/workflows/run_cyclic_colabfold.sh
@@ -1,9 +1,8 @@
 mkdir -p raw_results
 colabfold_batch sequence.fasta raw_results
 
-tar -czvf raw_results.tgz raw_results
-
 mkdir -p results
-$SCHRODINGER/run close_cycles.py raw_results results
+$SCHRODINGER/run close_cycles.py raw_results results --subdirs
 
+tar -czvf raw_results.tgz raw_results
 tar -czvf results.tgz results
