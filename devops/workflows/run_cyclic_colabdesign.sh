5564ac75829ca2354f1bcd27c30ec9800f1174c1
Griffin Young <griffin.young@schrodinger.com>
2023-10-17 13:12:08 -0400

    Moved colabdeisng install to correct bash script

--- devops/workflows/run_cyclic_colabdesign.sh
+++ devops/workflows/run_cyclic_colabdesign.sh
@@ -1,8 +1,24 @@
+#!/bin/bash
+
+$SCHRODINGER/run schrodinger_virtualenv.py venv
+source venv/bin/activate
+python -m pip -q install git+https://github.com/sokrypton/ColabDesign.git@v1.1.1
+
+# Try copying existing environment
+#gsutil -m cp -r gs://alphafold-environments/cyclic_peptide_design_environment/ params
+if ! [ -d params ]; then
+    mkdir params
+    wget --no_check_certificate https://storage.googleapis.com/alphafold/alphafold_params_2022-12-06.tar
+    tar -xf alphafold_params_2022-12-06.tar -C params
+    # Save the environment
+    #gsutil -m cp -r params gs://alphafold-environments/cyclic_peptide_design_environment
+fi
+
 mkdir -p raw_results
-python3 design.py $protocol raw_results --hallucination_length $hallucination_length --backbone_structures backbone_structures.maegz --backbone_chains backbone_chains.txt
+python design.py $protocol raw_results --hallucination_length $hallucination_length --backbone_structures backbone_structures.maegz --backbone_chains backbone_chains.txt
 
 mkdir -p results
-python3 close_cycles.py raw_results results
+python close_cycles.py raw_results results
 
 tar -czvf raw_results.tgz raw_results
 tar -czvf results.tgz results
