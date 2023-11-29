ecf987327a9bcabcfe9d7ff5ec74e8d712c70e0c
Griffin Young <griffin.young@schrodinger.com>
2023-11-29 12:02:41 -0500

    Matching jenkins file names

--- devops/workflows/run_cyclic_colabdesign.sh
+++ devops/workflows/run_cyclic_colabdesign.sh
@@ -16,9 +16,9 @@ fi
 
 mkdir -p raw_results
 if [[ -e "structures.maegz" ]]; then
-    python design.py $protocol raw_results --num_seqs $num_seqs --design_params design_params --structure_file structures.maegz
+    python design.py $protocol raw_results --num_seqs $num_seqs --design_parameters design_parameters.csv --structure_file structures.maegz
 else
-    python design.py $protocol raw_results --num_seqs $num_seqs --design_params design_params
+    python design.py $protocol raw_results --num_seqs $num_seqs --design_parameters design_parameters.csv
 fi
 
 mkdir -p results
