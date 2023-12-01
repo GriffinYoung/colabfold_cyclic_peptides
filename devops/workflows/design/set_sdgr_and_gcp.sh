c413319421eb3e723397cbb9f613c659a896b647
Griffin Young <griffin.young@schrodinger.com>
2023-12-01 14:42:37 -0500

    Attempt at working Jenkinsfile

--- devops/workflows/design/set_sdgr_and_gcp.sh
+++ devops/workflows/design/set_sdgr_and_gcp.sh
@@ -14,12 +14,5 @@ gcloud auth activate-service-account argo-workload@ddag-gke.iam.gserviceaccount.
 gcloud container clusters get-credentials alphafold-dev --zone us-central1-c --project ddag-gke
 
 # Get SCHRODINGER path
-export STABLE_BUILD='suite2023-3-build081'
-export SCHRODINGER="/mnt/squashsuites/${STABLE_BUILD}"
-ls $SCHRODINGER
-
-# Copy the environment from a bucket (or create it if the script hasn't been run before)
-source devops/workflows/install_colabdesign.sh
-
-# Run colabfold then bond the termini and protein prep
-source devops/workflows/run_cyclic_colabdesign.sh
+stable_build='suite2023-3-build081'
+export SCHRODINGER="/mnt/squashsuites/${stable_build}"
