12cd178383c9c39b361beeda95483448bca179d1
Griffin Young <griffin.young@schrodinger.com>
2023-12-04 16:29:09 -0500

    accessing params correctly (?)

--- devops/workflows/design/run_cyclic_colabdesign.sh
+++ devops/workflows/design/run_cyclic_colabdesign.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+protocol=$1
+num_seqs=$2
 
 if [[ -s "structures.maegz" ]]; then
     python3 design.py $protocol raw_results --num_seqs $num_seqs --design_parameters design_parameters.csv --structure_file structures.maegz
