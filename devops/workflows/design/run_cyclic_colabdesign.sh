237dae5f36b69a9d76719e2a5b855c56d91f9445
Griffin Young <griffin.young@schrodinger.com>
2023-12-04 14:13:55 -0500

    Checking structures.maegz not empty

--- devops/workflows/design/run_cyclic_colabdesign.sh
+++ devops/workflows/design/run_cyclic_colabdesign.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ -e "structures.maegz" ]]; then
+if [[ -s "structures.maegz" ]]; then
     python3 design.py $protocol raw_results --num_seqs $num_seqs --design_parameters design_parameters.csv --structure_file structures.maegz
 else
     python3 design.py $protocol raw_results --num_seqs $num_seqs --design_parameters design_parameters.csv
