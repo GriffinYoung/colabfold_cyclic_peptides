a51d42c5a8e3e40bc2bbecb2b3f72fd5f18f2a73
Griffin Young <griffin.young@schrodinger.com>
2023-12-05 16:20:46 -0500

    unbuffering python calls

--- devops/workflows/design/run_cyclic_colabdesign.sh
+++ devops/workflows/design/run_cyclic_colabdesign.sh
@@ -3,7 +3,7 @@ protocol=$1
 num_seqs=$2
 
 if [[ -s "structures.maegz" ]]; then
-    python3 design.py $protocol raw_results --num_seqs $num_seqs --design_parameters design_parameters.csv --structure_file structures.maegz
+    python3 -u design.py $protocol raw_results --num_seqs $num_seqs --design_parameters design_parameters.csv --structure_file structures.maegz
 else
-    python3 design.py $protocol raw_results --num_seqs $num_seqs --design_parameters design_parameters.csv
+    python3 -u design.py $protocol raw_results --num_seqs $num_seqs --design_parameters design_parameters.csv
 fi
