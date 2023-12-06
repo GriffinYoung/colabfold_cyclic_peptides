5de28ab8286c5baa29f3a5e2a86221c17524a86e
Griffin Young <griffin.young@schrodinger.com>
2023-12-05 19:35:32 -0500

    not in venv anymore

--- devops/workflows/design/close_cycles.sh
+++ devops/workflows/design/close_cycles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p results
-python3 close_cycles.py raw_results results --subdirs
+$SCHRODINGER/run close_cycles.py raw_results results --subdirs
 cp raw_results/*.sequence results
 
 tar -czvf results.tgz results
