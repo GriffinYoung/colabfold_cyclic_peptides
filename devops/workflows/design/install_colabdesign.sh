3902f8dcb7c863a69d8c0ad75373fe6bbdf31e02
Griffin Young <griffin.young@schrodinger.com>
2023-12-06 15:56:41 -0500

    jax cuda and upgrade cudnn

--- devops/workflows/design/install_colabdesign.sh
+++ devops/workflows/design/install_colabdesign.sh
@@ -2,7 +2,10 @@
 
 $SCHRODINGER/run schrodinger_virtualenv.py venv
 source venv/bin/activate
+
 python -m pip -q install dm-haiku==0.0.10 # pin because schrodinger's python 3.8 is incompatible with dm-haiku 0.0.11
+pip install "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html # From [here](N ) otherwise jax can't find the gpu
+pip install nvidia-cudnn-cu11
 python -m pip -q install git+https://github.com/sokrypton/ColabDesign.git@v1.1.1
 
 # Try copying existing environment
