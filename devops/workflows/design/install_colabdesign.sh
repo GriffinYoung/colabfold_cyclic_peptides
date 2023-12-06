c5590f2d3a73cb5e725553a3e83cf82f94641080
Griffin Young <griffin.young@schrodinger.com>
2023-12-06 16:12:04 -0500

    upgrade cudnn after colabdesign install

--- devops/workflows/design/install_colabdesign.sh
+++ devops/workflows/design/install_colabdesign.sh
@@ -4,9 +4,11 @@ $SCHRODINGER/run schrodinger_virtualenv.py venv
 source venv/bin/activate
 
 python -m pip -q install dm-haiku==0.0.10 # pin because schrodinger's python 3.8 is incompatible with dm-haiku 0.0.11
-pip install "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html # From [here](N ) otherwise jax can't find the gpu
-pip install nvidia-cudnn-cu11
 python -m pip -q install git+https://github.com/sokrypton/ColabDesign.git@v1.1.1
+pip install -U "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html # From [here](N ) otherwise jax can't find the gpu
+pip install -U nvidia-cudnn-cu11
+
+python3 -c "import jax; print(jax.devices())"
 
 # Try copying existing environment
 #gsutil -m cp -r gs://alphafold-environments/cyclic_peptide_design_environment/ params
