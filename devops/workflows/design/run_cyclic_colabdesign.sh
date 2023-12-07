ba004be3702621716786443e2d081220c9cd320b
Griffin Young <griffin.young@schrodinger.com>
2023-12-07 17:58:53 -0500

    the -> then

--- devops/workflows/design/run_cyclic_colabdesign.sh
+++ devops/workflows/design/run_cyclic_colabdesign.sh
@@ -4,7 +4,7 @@ num_seqs=$2
 
 mkdir pdb_dir
 
-if [[ -s "structures.maegz" ]]; the
+if [[ -s "structures.maegz" ]]; then
     $SCHRODINGER/run python3 -u split_maegz.py structures.maegz pdb_dir
 else
     python3 -u design.py $protocol raw_results --num_seqs $num_seqs --design_parameters design_parameters.csv --pdb_dir pdb_dir
