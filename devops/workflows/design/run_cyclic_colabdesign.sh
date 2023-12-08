775cf0b7979d802d0edc895d1d932fb16c26cee5
Griffin Young <griffin.young@schrodinger.com>
2023-12-08 13:47:45 -0500

    Take design.py out of else

--- devops/workflows/design/run_cyclic_colabdesign.sh
+++ devops/workflows/design/run_cyclic_colabdesign.sh
@@ -6,6 +6,5 @@ mkdir pdb_dir
 
 if [[ -s "structures.maegz" ]]; then
     $SCHRODINGER/run python3 -u split_maegz.py structures.maegz pdb_dir
-else
-    python3 -u design.py $protocol raw_results --num_seqs $num_seqs --design_parameters design_parameters.csv --pdb_dir pdb_dir
 fi
+python3 -u design.py $protocol raw_results --num_seqs $num_seqs --design_parameters design_parameters.csv --pdb_dir pdb_dir
