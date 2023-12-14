c0735ceab23521136ee476dd9b38a9f388c808a8
Griffin Young <griffin.young@schrodinger.com>
2023-12-14 16:28:13 -0600

    ls

--- devops/workflows/design/run_cyclic_colabdesign.sh
+++ devops/workflows/design/run_cyclic_colabdesign.sh
@@ -5,4 +5,7 @@ num_seqs=$2
 if [[ -s "structures.maegz" ]]; then
     $SCHRODINGER/run python3 -u split_maegz.py structures.maegz .
 fi
+ls -l
+ls -l ..
+
 python3 -u design.py $protocol raw_results --num_seqs $num_seqs --design_parameters design_parameters.csv
